08924c66534382d8bf77138c3a5bce2555b3c30e
JeonAYeong <39648964+jay2721@users.noreply.github.com>
2020-12-24 15:50:51 +0900

    ayeong

---
+++ BaekJoon/No_10773.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+
+int count = 0;
+int stack[100000];
+
+void push(int n) {
+	stack[count] = n;
+	count++;
+}
+
+void pop() {
+	count--;
+	stack[count] = 0;
+}
+
+int main() {
+	int input;
+	int sum = 0;
+	scanf("%d", &input);
+
+	int data[100000];
+
+	for (int i = 0; i < input; i++) {
+		scanf("%d", &data[i]);
+		if (data[i] == 0)
+			pop();
+		else
+			push(data[i]);
+	}
+
+	for (int i = 0; i < count; i++) {
+		sum += stack[i];
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
