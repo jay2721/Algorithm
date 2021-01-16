4827ada054c9c1f2177fbe07aacf2dc01d3da0eb
jay2721 <jay2721@naver.com>
2021-01-16 17:20:45 +0900

    ayeong

---
+++ BaekJoon/No_11654.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class No_11654 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		String word=sc.next();
+		int answer=word.charAt(0);
+		System.out.println(answer);
+	}
+}
