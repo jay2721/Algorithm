97427c7c37152be58107e84f06762e65cd7a7a55
jay2721 <jay2721@naver.com>
2021-01-26 15:01:29 +0900

    ayeong

---
+++ BaekJoon/No_10757.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+public class No_10757 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		BigInteger a=sc.nextBigInteger();
+		BigInteger b=sc.nextBigInteger();
+		BigInteger sum=a.add(b);
+		
+		System.out.println(sum);
+		
+	}
+}
