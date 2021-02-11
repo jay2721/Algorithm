fa95722f5a91ad11e8b10699bd0b9b0266b3e9a5
jay2721 <jay2721@naver.com>
2021-02-11 18:56:07 +0900

    ayeong

---
+++ Programmers/정렬/H-Index/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int h=0;
+        Arrays.sort(citations);
+        
+        for(int i=0; i<citations.length;i++){
+            h=citations.length-i;
+            
+            if(citations[i]>=h){
+                answer=h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
