daa348e71e44bc38a0c59288533a1e2b6ba8427b
jay2721 <jay2721@naver.com>
2022-04-01 16:13:06 +0900

    ayeong

---
+++ BaekJoon/No_10870.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class No_10870 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		System.out.println(fibo(n));
+		
+	}
+	
+	public static int fibo(int n) {
+		
+		if(n==0||n==1)
+			return n;
+		
+		return fibo(n-1)+fibo(n-2);
+	}
+
+}
