7b551693a37937f1293b52e7e46a8978c56ab05c
jay2721 <jay2721@naver.com>
2022-04-30 15:01:29 +0900

    ayeong

--- BaekJoon/No_1932.java
+++ BaekJoon/No_1932.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 public class No_1932 {
 
-	static int [][] arr;
-	static Integer[][] check;
+	static int [][] arr,check;
 	static int n;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner sc= new Scanner(System.in);
 		n=sc.nextInt();
 		
 		arr = new int[n][n];
-		check = new Integer [n][n];
+		check = new int [n][n];
 		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<i+1;j++) {
@@ -21,10 +19,15 @@ public class No_1932 {
 		}
 		
 		for(int i=0;i<n;i++) {
+			Arrays.fill(check[i], -1);
+			
+		}
+		
+		for(int i=0;i<n;i++) {
 			check[n-1][i]=arr[n-1][i];
 		}
 		
-
+		
 		System.out.println(find(0,0));
 
 	}
@@ -32,7 +35,7 @@ public class No_1932 {
 	public static int find(int depth, int i) {
 		if(depth==n-1)
 			return check[depth][i];
-		if(check[depth][i]==null)
+		if(check[depth][i]==-1)
 			check[depth][i]=Math.max(find(depth+1,i), find(depth+1,i+1))+arr[depth][i];
 		
 		return check[depth][i];
